ca588d1f884069a133d1db6d743f0860915ffd29
Pathos <29135741+Pathos14489@users.noreply.github.com>
2025-05-30 20:56:01 -0700

    updated message_formatter to allow for dedicated EOS strings for each message role type

--- message_formatter.py
+++ message_formatter.py
@@ -11,10 +11,13 @@ class PromptStyle(BaseModel):
     message_signifier: str = ": "
     role_seperator: str = "<|end_header_id|>\n\n"
     message_seperator: str = ""
-    message_format: str = "[BOS_token][role][role_seperator][name][message_signifier][content][EOS_token][message_seperator]"
+    message_format: str = "[BOS_token][role][role_seperator][name][message_signifier][content][specific_EOS_token][EOS_token][message_seperator]"
     system_name: str = "system"
     user_name: str = "user"
     assistant_name: str = "assistant"
+    system_EOS_token: str = ""
+    user_EOS_token: str = ""
+    assistant_EOS_token: str = ""
 
 class MessageTextContent(BaseModel):
     type: str = "text"
@@ -56,10 +59,13 @@ class MessageFormatter(): # Tokenizes(only availble for counting the tokens in a
         self.system_name = ps.system_name
         self.user_name = ps.user_name
         self.assistant_name = ps.assistant_name
+        self.system_EOS_token = ps.system_EOS_token
+        self.user_EOS_token = ps.user_EOS_token
+        self.assistant_EOS_token = ps.assistant_EOS_token
 
     def _version(self): # Returns the version of the message formatter
         """Returns the version of the message formatter"""
-        return "0.0.2"
+        return "0.0.4"
         
     def new_message(self, content, role, name=None): # Parses a string into a message format with the name of the speaker
         """Parses a string into a message format with the name of the speaker"""
@@ -89,13 +95,29 @@ class MessageFormatter(): # Tokenizes(only availble for counting the tokens in a
         if role == "":
             parsed_msg_part = parsed_msg_part.split("[role_seperator]")[0]
         parsed_msg_part = parsed_msg_part.replace("[BOS_token]",self.BOS_token)
-        parsed_msg_part = parsed_msg_part.replace("[role]",role)
+        formatted_roled = ""
+        if role == "system":
+            formatted_roled = self.system_name
+        elif role == "user":
+            formatted_roled = self.user_name
+        elif role == "assistant":
+            formatted_roled = self.assistant_name
+        parsed_msg_part = parsed_msg_part.replace("[role]",formatted_roled)
         parsed_msg_part = parsed_msg_part.replace("[role_seperator]",role_sep)
         parsed_msg_part = parsed_msg_part.replace("[name]",name)
         parsed_msg_part = parsed_msg_part.replace("[message_signifier]",msg_sig)
+        specific_EOS_token = ""
+        if role == "system":
+            specific_EOS_token = self.system_EOS_token
+        elif role == "user":
+            specific_EOS_token = self.user_EOS_token
+        elif role == "assistant":
+            specific_EOS_token = self.assistant_EOS_token
+        parsed_msg_part = parsed_msg_part.replace("[specific_EOS_token]",specific_EOS_token)
         parsed_msg_part = parsed_msg_part.replace("[EOS_token]",self.EOS_token)
         parsed_msg_part = parsed_msg_part.replace("[message_seperator]",self.message_seperator)
         parsed_msg_part = parsed_msg_part.split("[content]")[0]
+        print(f"Parsed message first part: {parsed_msg_part}")
         return parsed_msg_part
 
     def end_message(self, role="", name=None): # Returns the end of a message with the name of the speaker (Incase the message format chosen requires the name be on the end for some reason, but it's optional to include the name in the end message)
@@ -113,17 +135,34 @@ class MessageFormatter(): # Tokenizes(only availble for counting the tokens in a
         if role == "":
             parsed_msg_part = parsed_msg_part.split("[role_seperator]")[1]
         parsed_msg_part = parsed_msg_part.replace("[BOS_token]",self.BOS_token)
-        parsed_msg_part = parsed_msg_part.replace("[role]",role)
+        formatted_roled = ""
+        if role == "system":
+            formatted_roled = self.system_name
+        elif role == "user":
+            formatted_roled = self.user_name
+        elif role == "assistant":
+            formatted_roled = self.assistant_name
+        parsed_msg_part = parsed_msg_part.replace("[role]",formatted_roled)
         parsed_msg_part = parsed_msg_part.replace("[role_seperator]",role_sep)
         parsed_msg_part = parsed_msg_part.replace("[name]",name)
         parsed_msg_part = parsed_msg_part.replace("[message_signifier]",msg_sig)
+        specific_EOS_token = ""
+        if role == "system":
+            specific_EOS_token = self.system_EOS_token
+        elif role == "user":
+            specific_EOS_token = self.user_EOS_token
+        elif role == "assistant":
+            specific_EOS_token = self.assistant_EOS_token
+        parsed_msg_part = parsed_msg_part.replace("[specific_EOS_token]",specific_EOS_token)
         parsed_msg_part = parsed_msg_part.replace("[EOS_token]",self.EOS_token)
         parsed_msg_part = parsed_msg_part.replace("[message_seperator]",self.message_seperator)
         parsed_msg_part = parsed_msg_part.split("[content]")[1]
+        print(f"Parsed message second part: {parsed_msg_part}")
         return parsed_msg_part
 
     def get_string_from_messages(self, messages: list[Message]): # Returns a formatted string from a list of messages
         """Returns a formatted string from a list of messages"""
+        print(f"Using message format: {self.message_format}")
         context = ""
         images = []
         print(f"Creating string from messages: {len(messages)}")
@@ -165,6 +204,8 @@ class MessageFormatter(): # Tokenizes(only availble for counting the tokens in a
                     name = None
             msg_string = self.new_message(content, role, name)
             context += msg_string
+        print(f"Context:")
+        print(context)
         return context, images
     
     def __str__(self):
